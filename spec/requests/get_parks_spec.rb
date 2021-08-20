453aaddc4ac2a670da934a8f005652766719bbee
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 16:17:37 -0700

    add route test for finding one park

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -14,3 +14,20 @@ describe "get all parks route", :type => :request do
   end
 
 end
+
+describe "get a park or search route", :type => :request do
+
+  before do
+    park = Park.create!({ :park_name => 'test_name', :description => 'test_description', :park_type => "National Park", :state => 'test_state', :city => 'test_city', })
+    get "/parks/#{park.id}"
+  end
+
+  it 'returns the park name at that id' do
+    expect(JSON.parse(response.body)['park_name']).to eq('Test Name')
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+end
