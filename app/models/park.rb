1464dae76400ce17032d51976c85f9564fe01fe9
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 09:53:34 -0700

    correct syntax of scopes

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,8 +1,7 @@
 class Park < ApplicationRecord
-  require: park_name:, state:, city:, description:, park_type:
-  scope :state -> (state_parameter) {where('LOWER(state) like ?', "%#{state_parameter.downcase}%")}
-  scope :park_type -> (type_parameter) {where('LOWER(park_type) like ?', "%#{type_parameter.downcase}%")}
-  
+  # require: park_name:, state:, city:, description:, park_type:
+  scope :state_search, -> (state_parameter) {where('LOWER(state) like ?', "%#{state_parameter.downcase}%")}
+  scope :type_search, -> (type_parameter) {where('LOWER(park_type) like ?', "%#{type_parameter.downcase}%")}
   def self.state_by_type(state, park_type)
     where('LOWER(state) like ?', parameter1.downcase).and(Review.where('LOWER(park_type) like ?', parameter2.downcase))
   end
