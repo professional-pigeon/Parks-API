cf10d6f2873be3ef1f85e17d3a128e20b2d65e6e
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 09:01:13 -0700

    add controller logic for CRUD functionality

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -4,8 +4,33 @@ class ParksController < ApplicationController
     json_response(@parks)
   end
 
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @parks = Park.create(park_params)
+    json_response(@parks)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    @park.update(park_params)
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    @park.destroy
+  end
+
   private
   def json_response(object, status = :ok)
     render json: object, status: status
   end
+
+  def park_params
+    params.permit(:park_name, :park_type, :state, :city, :description)
+  end
+
 end
