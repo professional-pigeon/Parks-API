7e7516fcc1bc2de050ab6a18121d79de6618fd0e
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 14:19:16 -0700

    create and pass tests for destroying a Park

--- spec/requests/change_parks_spec.rb
+++ spec/requests/change_parks_spec.rb
@@ -15,3 +15,20 @@ describe "patch a park route", :type => :request do
     expect(response).to have_http_status(:ok)
   end
 end
+
+describe "delete a park route", :type => :request do
+  
+  before do
+    park = Park.create!({ :park_name => 'test_name', :description => 'test_description', :park_type => "National Park", :state => 'test_state', :city => 'test_city', })
+    delete "/parks/#{park.id}"
+  end
+
+  it 'to tell you the park has been destroyed' do
+    expect(JSON.parse(response.body)["message"]).to eq("This park has been destroyed successfully")
+  end
+
+  it 'returns a created status' do
+    expect(response).to have_http_status(:ok)
+  end
+end
+
