397c08ae5930f38add6690ea623dab47188e862b
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 08:53:10 -0700

    Create seed for for parks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_parks
+  end
+
+  def generate_parks
+    20.times do |i|
+      ticker = 2
+      if ticker % 2 == 0
+        state_national = "State park"
+      else
+        state_national = "National park"
+      end
+      ticker += 1
+      parks = Park.create!(
+        park_name: Faker::Name.last_name,
+        state: Faker::Address.state,
+        city: Faker::Address.city,
+        park_type: state_national,
+        description: Faker::Lorem.words(number: 20)
+      )
+    end
+    puts "Parks created."
+  end
+end
+
+Seed.begin
