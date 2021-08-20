c8922474fa8e9aafba7ed915418872ed8f39c6bb
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 16:21:24 -0700

    add test for searching by state

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -31,3 +31,20 @@ describe "get a park or search route", :type => :request do
   end
 
 end
+
+describe "get a park by state", :type => :request do
+
+  before do
+    park = Park.create!({ :park_name => 'test_name', :description => 'test_description', :park_type => "National Park", :state => 'wisconsin', :city => 'test_city', })
+    get "/parks?state=wisconsin"
+  end
+
+  it 'returns the park name at that id' do
+    expect(JSON.parse(response.body)[0]['state']).to eq('Wisconsin')
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+end
