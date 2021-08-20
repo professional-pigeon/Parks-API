3fdf2096b993d14a90d1aa230221247d2c75a22f
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 16:23:30 -0700

    add test for searching by state and type

--- spec/requests/get_parks_spec.rb
+++ spec/requests/get_parks_spec.rb
@@ -39,7 +39,7 @@ describe "get a park by state", :type => :request do
     get "/parks?state=wisconsin"
   end
 
-  it 'returns the park name at that id' do
+  it 'returns the park state related to the search params' do
     expect(JSON.parse(response.body)[0]['state']).to eq('Wisconsin')
   end
 
@@ -56,8 +56,28 @@ describe "get a park by type", :type => :request do
     get "/parks?park_type=national park"
   end
 
+  it 'returns the park type related to the search params' do
+    expect(JSON.parse(response.body)[0]['park_type']).to eq('National Park')
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+end
+
+describe "get a park by type and state", :type => :request do
+
+  before do
+    park = Park.create!({ :park_name => 'test_name', :description => 'test_description', :park_type => "National Park", :state => 'wisconsin', :city => 'test_city', })
+    get "/parks?park_type=national park"
+  end
+
   it 'returns the park name at that id' do
     expect(JSON.parse(response.body)[0]['park_type']).to eq('National Park')
+  end
+  it 'returns the park state related to the search params' do
+    expect(JSON.parse(response.body)[0]['state']).to eq('Wisconsin')
   end
 
   it 'returns status code 200' do
