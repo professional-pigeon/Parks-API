fd97db212a1317bee92fb3f09a2a913631b1e7a6
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 15:20:51 -0700

    titleize elements of Park on save

--- app/models/park.rb
+++ app/models/park.rb
@@ -6,4 +6,14 @@ class Park < ApplicationRecord
   validates :state, presence: true
   scope :state_search, -> (state_parameter) {where('LOWER(state) like ?', "%#{state_parameter.downcase}%")}
   scope :type_search, -> (type_parameter) {where('LOWER(park_type) like ?', "%#{type_parameter.downcase}%")}
+
+  before_save(:titleize_park)
+
+  private
+  def titleize_park
+    self.park_name = self.park_name.titleize
+    self.state = self.state.titleize
+    self.city = self.city.titleize
+    self.park_type = self.park_type.titleize
+  end
 end
