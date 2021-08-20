30e5d2991f2ef7ddd270740d2dbca4a0324454d6
professional-pigeon <kyle.kayperez@gmail.com>
2021-08-20 08:32:21 -0700

    migrate parks table

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,2 +1,11 @@
 class ParksController < ApplicationController
+  def index
+    @parks = {"quotation": "The secret of getting ahead is getting started."}
+    json_response(@quotes)
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
 end
--- db/migrate/20210820150943_add_parks_table.rb
+++ db/migrate/20210820150943_add_parks_table.rb
@@ -1,4 +1,11 @@
 class AddParksTable < ActiveRecord::Migration[5.2]
   def change
+    create_table :parks do |t|
+      t.column :park_name, :string
+      t.column :state, :string
+      t.column :city, :string
+      t.column :park_type, :string
+      t.column :description, :string
+    end
   end
 end
